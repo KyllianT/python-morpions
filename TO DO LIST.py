cd9fbcdadc0073f2812b38f5763c263f13ce4686
KyllianT <117281228+KyllianT@users.noreply.github.com>
2022-11-15 18:06:46 +0100

    Add files via upload

---
+++ TO DO LIST.py
@@ -0,0 +1,10 @@
+TO DO LIST
+
+    #penser à regler les pbs de fin (recommencer une game si le joueur veut)
+    #faire un mode IA (JvsIA) (mode JvsJ est deja fait juste le metre dans un if)
+    #regler les problèmes de chiffre non present dans la grille (intervalle de 3 a X )
+    #si on met 1 seul chiffre message d'erreur
+    #faire des regles au debut de la partie 
+    #faire un choix des joueur (choisit de jouer contre un autre J ou un IA)
+    #faire egalité quand toutes les cases sont deja prises
+
