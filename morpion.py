7f0a2b0afd9b186bb0ce9aa331e1870b9bb56ac8
Airtox <117281228+KyllianT@users.noreply.github.com>
2022-11-24 20:20:41 +0100

    Update morpion.py

--- morpion.py
+++ morpion.py
@@ -1,4 +1,11 @@
+#importer la bibliothéque time
 import time
+from math import inf as infinity
+from random import choice
+import platform
+import time
+from os import system
+
 
 #dictionnaire associant une valeur à chaque symbole
 valeur = {' ':0,'O':3,'X':5}
@@ -182,9 +189,240 @@ def JcJ():
             exit()
 
 def JcIA():
-    print("eifjgyehf")
-    
 
+    J = -1
+    IA = +1
+    board = [
+        [0, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0],
+    ]
+
+
+    def evaluate(state):
+        if wins(state, IA):
+            score = +1
+        elif wins(state, J):
+            score = -1
+        else:
+            score = 0
+
+        return score
+
+
+    def wins(state, player):
+        win_state = [
+            [state[0][0], state[0][1], state[0][2]],
+            [state[1][0], state[1][1], state[1][2]],
+            [state[2][0], state[2][1], state[2][2]],
+            [state[0][0], state[1][0], state[2][0]],
+            [state[0][1], state[1][1], state[2][1]],
+            [state[0][2], state[1][2], state[2][2]],
+            [state[0][0], state[1][1], state[2][2]],
+            [state[2][0], state[1][1], state[0][2]],
+        ]
+        if [player, player, player] in win_state:
+            return True
+        else:
+            return False
+
+
+    def game_over(state):
+        return wins(state, J) or wins(state, IA)
+
+
+    def empty_cells(state):
+        cells = []
+        for x, row in enumerate(state):
+            for y, cell in enumerate(row):
+                if cell == 0:
+                    cells.append([x, y])
+
+        return cells
+
+
+    def valid_move(x, y):
+        if [x, y] in empty_cells(board):
+            return True
+        else:
+            return False
+
+
+    def set_move(x, y, player):
+        if valid_move(x, y):
+            board[x][y] = player
+            return True
+        else:
+            return False
+
+
+    def minimax(state, depth, player):
+        if player == IA:
+            best = [-1, -1, -infinity]
+        else:
+            best = [-1, -1, +infinity]
+
+        if depth == 0 or game_over(state):
+            score = evaluate(state)
+            return [-1, -1, score]
+
+        for cell in empty_cells(state):
+            x, y = cell[0], cell[1]
+            state[x][y] = player
+            score = minimax(state, depth - 1, -player)
+            state[x][y] = 0
+            score[0], score[1] = x, y
+
+            if player == IA:
+                if score[2] > best[2]:
+                    best = score  # max value
+            else:
+                if score[2] < best[2]:
+                    best = score  # min value
+
+        return best
+
+
+    def clean():
+        os_name = platform.system().lower()
+        if 'windows' in os_name:
+            system('cls')
+        else:
+            system('clear')
+
+    def render(state, c_choice, h_choice):
+        chars = {
+            -1: h_choice,
+            +1: c_choice,
+            0: ' '
+        }
+        str_line = '---------------'
+
+        print('\n' + str_line)
+        for row in state:
+            for cell in row:
+                symbol = chars[cell]
+                print(f'| {symbol} |', end='')
+            print('\n' + str_line)
+
+
+    def ia_turn(c_choice, h_choice):
+        depth = len(empty_cells(board))
+        if depth == 0 or game_over(board):
+            return
+
+        clean()
+        print(f'IA Joue [{c_choice}]')
+        render(board, c_choice, h_choice)
+
+        if depth == 9:
+            x = choice([0, 1, 2])
+            y = choice([0, 1, 2])
+        else:
+            move = minimax(board, depth, IA)
+            x, y = move[0], move[1]
+
+        set_move(x, y, IA)
+        time.sleep(1)
+
+
+    def human_turn(c_choice, h_choice):
+        depth = len(empty_cells(board))
+        if depth == 0 or game_over(board):
+            return
+
+        # Dictionary of valid moves
+        move = -1
+        moves = {
+            1: [0, 0], 2: [0, 1], 3: [0, 2],
+            4: [1, 0], 5: [1, 1], 6: [1, 2],
+            7: [2, 0], 8: [2, 1], 9: [2, 2],
+        }
+
+        clean()
+        print(f'A vous de jouer [{h_choice}]')
+        render(board, c_choice, h_choice)
+
+        while move < 1 or move > 9:
+            try:
+                move = int(input('numéro ? (1..9): '))
+                coord = moves[move]
+                can_move = set_move(coord[0], coord[1], J)
+
+                if not can_move:
+                    print('mauvaise saisis')
+                    move = -1
+            except (EOFError, KeyboardInterrupt):
+                print('Adieu')
+                exit()
+            except (KeyError, ValueError):
+                print('Mauvais choix')
+
+
+    def main():
+        clean()
+        h_choice = ''  # X or O
+        c_choice = ''  # X or O
+        first = ''  # if human is the first
+
+ 
+        while h_choice != 'O' and h_choice != 'X':
+            try:
+                print('')
+                h_choice = input('Choisit X ou O\nChoix : ').upper()
+            except (EOFError, KeyboardInterrupt):
+                print('Adieu')
+                exit()
+            except (KeyError, ValueError):
+                print('Mauvais choix')
+
+ 
+        if h_choice == 'X':
+            c_choice = 'O'
+        else:
+            c_choice = 'X'
+
+
+        clean()
+        while first != 'Y' and first != 'N':
+            try:
+                first = input('Tu veux commencer ?[y/n]: ').upper()
+            except (EOFError, KeyboardInterrupt):
+                print('Adieu')
+                exit()
+            except (KeyError, ValueError):
+                print('Mauvais choix')
+
+
+        while len(empty_cells(board)) > 0 and not game_over(board):
+            if first == 'N':
+                ia_turn(c_choice, h_choice)
+                first = ''
+
+            human_turn(c_choice, h_choice)
+            ia_turn(c_choice, h_choice)
+
+        # Game over message
+        if wins(board, J):
+            clean()
+            print(f'A toi de jouer[{h_choice}]')
+            render(board, c_choice, h_choice)
+            print('TU AS GAGNER!')
+        elif wins(board, IA):
+            clean()
+            print(f'IA joue [{c_choice}]')
+            render(board, c_choice, h_choice)
+            print('TU AS PERDU !')
+        else:
+            clean()
+            render(board, c_choice, h_choice)
+            print('EGALITE !')
+
+        exit()
+
+
+    if __name__ == '__main__':
+        main()
 
 
 #################################################    Regle du morpion    ################################################# 
@@ -212,6 +450,4 @@ def regle_morpion() :
        print ("Félicitation vous avez choisi de jouer contre moi je vais te briser ! ")
        time.sleep (2)
        JcIA()
-
-
 regle_morpion()
